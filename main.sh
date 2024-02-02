1fda0ed25483e5a70dc46e29e4956649c3c08fe7
Serezha <102390317+SergeiTregub@users.noreply.github.com>
2024-02-02 21:34:19 +0300

    Pre-release update

--- main.sh
+++ main.sh
@@ -2,7 +2,10 @@ echo WELCOME TO DRAGONHOST
 echo The Original Project by SergeiTregub on GitHub
 echo Local hosting websites.
 sleep 5
+python welcome.py
+python read.py
 echo NOW ATTEMPTING TO HOST A LOCAL WEBSITE
 python3 -m http.server 8080
 echo Hosting closed now.
 echo Thank you for using DragonHost!
+sleep 2
